87daa1b1375e385ce0c1bbc0d2b6027b8644d676
Guido Zavagli <guido@zavagli.com>
2018-09-15 07:43:50 -0700

    First functional version of this app

--- app/src/main/java/com/zavagli/weightconverter/MainActivity.java
+++ app/src/main/java/com/zavagli/weightconverter/MainActivity.java
@@ -5,25 +5,53 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "WeightConverter";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        final EditText editText1 = findViewById(R.id.editTextWeight1);
+        final EditText editText2 = findViewById(R.id.editTextWeight2);
+        editText1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d(TAG, "onTextChanged");
+                float weightPounds = 0;
+                if (editText1.getText().length() > 0) {
+                    weightPounds = Float.valueOf(editText1.getText().toString());
+                }
+                if (weightPounds > 0) {
+                    editText2.setText(Float.toString(weightPounds * 0.453592f));
+                }
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
     }
